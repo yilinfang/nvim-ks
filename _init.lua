e5fa464f17fe2b4137a9c19bb0edb88ea370ac27
Yilin Fang <qzfyl98@outlook.com>
2025-04-12 17:42:24 -0400

    Add formater for html, css and javascript

--- _init.lua
+++ _init.lua
@@ -857,7 +857,10 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
         -- HACK: Other formatters
+        css = { 'prettier' },
         fish = { 'fish_indent' },
+        html = { 'prettier' },
+        javascript = { 'prettier' },
         sh = { 'shfmt' },
         python = {
           -- To fix auto-fixable lint errors.
